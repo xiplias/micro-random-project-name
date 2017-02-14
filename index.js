2f87f60cc65be0d1497616967d60f3a4c6ff7611
Anders Hansen <xiplias@gmail.com>
2017-02-14 12:50:13 +0100

    Adding CORS support

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var generate = require('project-name-generator');
+const generate = require('project-name-generator');
+const { send } = require('micro')
 
-module.exports = (req, res) => generate({ words: 2, alliterative: true }).spaced
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  const data = generate({ words: 2, alliterative: true }).spaced;
+
+  send(res, 200, data)
+}
